3a6fca9150c42d2360ba664df0c429dcb098e5e9
Aashutosh Soni <omsoni051@gmail.com>
2025-05-18 18:08:21 -0400

    Update Netlify function configuration and API endpoints

--- src/components/Generate.jsx
+++ src/components/Generate.jsx
@@ -14,7 +14,7 @@ import {
 import ProjectResults from "./ProjectResults";
 
 const API_URL = import.meta.env.PROD
-  ? "/api/generate-projects"
+  ? "/.netlify/functions/generate-projects"
   : "http://localhost:3001/api/generate-projects";
 
 const Generate = () => {
